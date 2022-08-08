0acec91535aebf2f8e3cfd025a36b6b527c9038b
MatiasF <83260164+mattkaizen@users.noreply.github.com>
2022-08-08 14:44:38 -0300

    AudioBoss 1
    Se incorporo del boss:
    1_Sonido de ataque 3D
    2-Sonido de grito 3D al cambiar de fase

--- Assets/2.Scripts/Personaje Principal/Puzzle/PlayerInventory.cs
+++ Assets/2.Scripts/Personaje Principal/Puzzle/PlayerInventory.cs
@@ -7,14 +7,36 @@ public class PlayerInventory : MonoBehaviour
     [Header("Inventario del jugador")]
     [SerializeField] List<GameObject> inventoryGOs;
 
+    private AudioBoss audioBoss;
     public List<GameObject> InventoryGOs { get => inventoryGOs; }
+
+    private void Awake()
+    {
+        audioBoss = FindObjectOfType<AudioBoss>();
+    }
     public void AddPuzzle(GameObject go)
     {
         inventoryGOs.Add(go);
+        ChangeGamePhase();
     }
 
     public void RemovePuzzle(GameObject go)
     {
         inventoryGOs.Remove(go);
+    }
+
+    private void ChangeGamePhase() //Tal vez deba ser colocado en gameManager
+    {
+        switch(inventoryGOs.Count)
+        {
+            case 3:
+                GameEnvironment.Singleton.PhaseNumber = 2;
+                audioBoss.PlayGrowlPhase(GameEnvironment.Singleton.PhaseNumber);
+                break;
+            case 6:
+                GameEnvironment.Singleton.PhaseNumber = 3;
+                audioBoss.PlayGrowlPhase(GameEnvironment.Singleton.PhaseNumber);
+                break;
+        }
     }
 }
